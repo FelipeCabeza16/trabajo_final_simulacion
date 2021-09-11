9c83e9647a19e12d85979dfee2811e7fdc7394a6
felipecabeza16 <felipecabezas98@gmail.com>
2021-09-11 11:36:49 -0500

    Crea el dataset inicial

--- boltzmann.R
+++ boltzmann.R
@@ -5,7 +5,7 @@ library(ggplot2)
 library(gganimate)
 #inicializar los valores 
 
-particles = 1000
+n_particles = 1000
 #Atomo de Hidrógeno 
 r = 120e-12
 #Temeratura constante
@@ -13,32 +13,38 @@ T = 300
 FPS = 30
 dt = 1/FPS
 
-sx = runif(particles)
-sy = runif(particles)
-
-s = array(c(sx, sy), dim = c(particles, 2))
+sx = runif(n_particles)
+sy = runif(n_particles)
 
 #Velocidad máxima
 v_max = 1.5
 
-thetha = runif(particles) * 2 * pi
+thetha = runif(n_particles) * 2 * pi
 vx = v_max * cos(thetha)
 vy = v_max * sin(thetha)
-
-v = array(c(vx, vy), dim = c(particles, 2))
 
 time_max = 5
 dt = 0.1
 
-forward = function(dt, time_max){
+
+#retorna el dataset con los datos iniciales 
+init_particles = function(){
+  particles = data.frame(sx, sy, vx, vy)
+  names(particles) <- c('Sx', 'Sy','Vx', 'Vy')
+  return(particles)
+}
+
+forward = function(dt, time_max, particles){
 
 time = 0
 
 pos = array()
   while(time <= time_max){
     time = time + dt  
-    s = s + v*dt
+    sx = sx + vx*dt
+    sy = sy + vy*dt
   }
 } 
-  
-forward(dt, time_max)
+
+particles = init_particles()  
+#forward(dt, time_max)
