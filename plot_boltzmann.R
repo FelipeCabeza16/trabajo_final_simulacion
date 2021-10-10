fe30a7946b4e3bb11c98299c825d50b9d27e0dce
GaVO <0vega.gabriel@gmail.com>
2021-10-10 15:05:06 -0500

    Add color and time to plot 	modified:   plot_boltzmann.R

--- plot_boltzmann.R
+++ plot_boltzmann.R
@@ -12,8 +12,10 @@ library(magick)
 #cambiar la ruta absoluta por la de su sistema, chmod 777
 # y las dos simulaciones están en los datasets (particles_n_n.csv) o (particles_2_box.csv)
 
-dataset1="particles_2_box.csv"
-par = read.csv("D:\\Proyects\\R\\simulacion\\trabajo_final_simulacion\\dataset\\"+dataset1)
+#install.packages('rstudioapi')
+directory = dirname(rstudioapi::getActiveDocumentContext()$path)
+dataset1="/dataset/particles_2_box.csv"
+par = read.csv(paste(directory,dataset1,sep = ""))
 
 #par = read.csv(file = "/home/felipe/Documentos/Programming/R/Simulación/Trabajo_Final/Trabajo_final/dataset/particles_n_n.csv")
 
@@ -32,7 +34,6 @@ r <- 120e-12*escalaR
 #Velocidad de la simulación
 totalv <- sqrt((par$Vx^2)+(par$Vy^2))
 totalv <- tail( totalv, n_par)
-
 
 x_graf <- seq(min(totalv*(escalaT/escalaR)),max(totalv*(escalaT/escalaR)),by=0.01)
 
@@ -75,31 +76,37 @@ simOneBox <- function(par,tam_box){
 }
 
 
-#Velocidad analítica
-x_graf <- seq(min(totalv*(escalaT/escalaR)),max(totalv*(escalaT/escalaR)),by=0.01)
-sigm <- sqrt( k_botlz*Temp / m  )
-y_graf <-  ( x_graf / (sigm^2) )* exp( - (  x_graf^2 / (2*sigm^2) ) ) 
+teorical <- function(par){
+  #Velocidad analítica
+  x_graf <- seq(min(totalv*(escalaT/escalaR)),max(totalv*(escalaT/escalaR)),by=0.01)
+  sigm <- sqrt( k_botlz*Temp / m  )
+  y_graf <-  ( x_graf / (sigm^2) )* exp( - (  x_graf^2 / (2*sigm^2) ) ) 
+  
+  
+  data <- data.frame(cbind(x_graf,y_graf))
+  
+  
+  graph <- ggplot(par, aes((sqrt((par$Vx^2)+(par$Vy^2)))*(escalaT/escalaR))) + 
+    geom_histogram(col = "black",fill = "purple", aes(y=..density..)) + 
+    view_follow(fixed_x = TRUE, fixed_y = TRUE) + transition_time(par$time)  + 
+    geom_line(aes(x=x_graf,y=y_graf),data=data,col = "blue",size=1) + 
+    ggtitle("Simulacion vs solucion analitica", ) + 
+    labs(x = "Velocidad [m/s]", y = "f(v)", col="Solucion analitica",  caption ="Time: {round(frame_time,1)}") +
+    #scale_color_manual(labels = c("f(v)"), values = c("red"))
+    theme(axis.title.y=element_blank(), axis.text.y=element_blank(), axis.ticks.y=element_blank(), 
+          legend.title = element_blank(), legend.text = element_text(size = 14),
+          plot.title = element_text(size = 12, face = "bold"), legend.position = "none")
+  return (graph)
+}
 
 
-data <- data.frame(cbind(x_graf,y_graf))
 
-
-graph <- ggplot(par, aes((sqrt((par$Vx^2)+(par$Vy^2)))*(escalaT/escalaR))) + 
-              geom_histogram(col = "black",fill = "purple", aes(y=..density..)) + 
-              view_follow(fixed_x = TRUE, fixed_y = TRUE) + transition_time(par$time)  + 
-              geom_line(aes(x=x_graf,y=y_graf),data=data) + 
-              ggtitle("Simulacion vs solucion analitica", ) + 
-              labs(x = "Velocidad [m/s]", y = "f(v)", col="Solucion analitica") +
-              #scale_color_manual(labels = c("f(v)"), values = c("red"))
-              theme(axis.title.y=element_blank(), axis.text.y=element_blank(), axis.ticks.y=element_blank(), 
-                    legend.title = element_blank(), legend.text = element_text(size = 14),
-                    plot.title = element_text(size = 12, face = "bold"))
-
+graph <- teorical(par)
 #Una caja NxN
-simu <- simOneBox(par,tam_box)
+#simu <- simOneBox(par,tam_box)
 
 #Dos cajas
-#simu <- simTwoBox(par,tam_box)
+simu <- simTwoBox(par,tam_box)
 
 
 
@@ -121,7 +128,7 @@ a_mgif <- image_read(a_gif)
 b_mgif <- image_read(b_gif)
 
 new_gif <- image_append(c(a_mgif[1], b_mgif[1]))
-for(i in 2:100){
+for(i in 2:length(a_mgif)){
   combined <- image_append(c(a_mgif[i], b_mgif[i]))
   new_gif <- c(new_gif, combined)
 }
